36848598f12db872f7b3c7e12d1dc273cd91e327
morteify <michal.weglarz@protonmail.com>
2020-03-19 17:59:00 +0100

    Add base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
-import LayoutWrapper from "./components/LayoutWrapper/LayoutWrapper";
+import SideMenu from "./components/SideMenu/SideMenu";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <LayoutWrapper />
-    </div>
+    <Router>
+      <div className="App">
+        <SideMenu />
+        <Switch>
+          <Route exact path="/">
+            <div>home</div>
+          </Route>
+          <Route path="/home">
+            <div>home</div>
+          </Route>
+          <Route path="/songs">
+            <div>songs</div>
+          </Route>
+          <Route path="/albums">
+            <div>albums</div>
+          </Route>
+          <Route path="/artists">
+            <div>artists</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
