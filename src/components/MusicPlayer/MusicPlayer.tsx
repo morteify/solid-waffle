8243b47736f872a2bbcf9d6a05cc81bf50cba653
morteify <michal.weglarz@protonmail.com>
2020-04-15 02:49:47 +0200

    Add basic slider music control

--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -9,7 +9,7 @@ import { RootReducer } from '../../redux/features/root';
 import useMusicPlayer from '../../hooks/useMusicPlayer';
 import styled from 'styled-components';
 import { Avatar } from 'antd';
-import { Slider, Switch } from 'antd';
+import { Slider, Row, Col } from 'antd';
 
 const Container = styled.div`
   height: 5rem;
@@ -17,8 +17,6 @@ const Container = styled.div`
   position: fixed;
   z-index: 2;
   bottom: 0;
-  display: flex;
-  flex-direction: row;
   background-color: coral;
 `;
 
@@ -41,47 +39,84 @@ function MusicPlayer(): JSX.Element {
   const dispatch = useDispatch();
   const currentSoundName = useSelector((state: RootReducer) => state.musicPlayer.soundName);
   const currentSongCover = useSelector((state: RootReducer) => state.musicPlayer.albumCover);
-  const [howlerSound, volume, setVolume, toggleSoundMute] = useMusicPlayer();
+  const isSongLoaded = useSelector((state: RootReducer) => !state.musicPlayer.isLoading);
+  const [
+    howlerSound,
+    soundID,
+    volume,
+    setVolume,
+    toggleSoundMute,
+    currentSoundPosition,
+    handleCurrentSoundPosition,
+    songDuration,
+  ] = useMusicPlayer();
+  console.log(songDuration);
+  console.log('current', currentSoundPosition);
   return (
     <Container>
-      <AlbumCoverContainer>
-        {currentSongCover ? <AlbumCover shape="square" src={currentSongCover} /> : <p>{"Song's album cover"}</p>}
-      </AlbumCoverContainer>
-      <div
-        style={{ width: '100px', height: '100px', backgroundColor: 'green' }}
-        onClick={(): void => {
-          dispatch(playMusic());
-        }}
-      >
-        Play
-      </div>
-      <div
-        style={{ width: '100px', height: '100px', backgroundColor: 'red' }}
-        onClick={(): void => {
-          dispatch(pauseMusic());
-        }}
-      >
-        Pause
-      </div>
-      <div
-        style={{ width: '100px', height: '100px', backgroundColor: 'pink' }}
-        onClick={(): void => {
-          toggleSoundMute();
-        }}
-      >
-        Mute
-      </div>
-      <div style={{ width: '100px', height: '100px', backgroundColor: 'blue' }}>
-        Volume: {volume}
-        <Slider
-          defaultValue={0.5}
-          min={0}
-          max={1}
-          step={0.01}
-          value={volume}
-          onChange={(value: any): void => setVolume(value as number)}
-        />
-      </div>
+      <Row>
+        <Col span={8}>
+          <AlbumCoverContainer>
+            {currentSongCover ? <AlbumCover shape="square" src={currentSongCover} /> : <p>{"Song's album cover"}</p>}
+            {currentSoundName}
+          </AlbumCoverContainer>
+        </Col>
+        <Col span={8}>
+          <Row>
+            <div
+              style={{ width: '100px', height: '100px', backgroundColor: 'green' }}
+              onClick={(): void => {
+                dispatch(playMusic());
+              }}
+            >
+              Play
+            </div>
+            <div
+              style={{ width: '100px', height: '100px', backgroundColor: 'red' }}
+              onClick={(): void => {
+                dispatch(pauseMusic());
+              }}
+            >
+              Pause
+            </div>
+            <div style={{ width: '100px', height: '100px', backgroundColor: 'blue' }}>
+              <Slider
+                defaultValue={1}
+                min={1}
+                max={songDuration}
+                step={1}
+                value={currentSoundPosition}
+                onChange={(value: number | [number, number]): void => handleCurrentSoundPosition(value as number)}
+              />
+
+              <div>{songDuration && songDuration}</div>
+            </div>
+          </Row>
+        </Col>
+        <Col span={8}>
+          <Row>
+            <div
+              style={{ width: '100px', height: '100px', backgroundColor: 'pink' }}
+              onClick={(): void => {
+                toggleSoundMute();
+              }}
+            >
+              Mute
+            </div>
+            <div style={{ width: '100px', height: '100px', backgroundColor: 'blue' }}>
+              Volume: {volume}
+              <Slider
+                defaultValue={0.5}
+                min={0}
+                max={1}
+                step={0.01}
+                value={volume}
+                onChange={(value: number | [number, number]): void => setVolume(value as number)}
+              />
+            </div>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/hooks/useMusicPlayer.ts
+++ src/hooks/useMusicPlayer.ts
@@ -9,14 +9,48 @@ interface MusicPlayer {
   songURL: string;
 }
 
-function useMusicPlayer(): [Howl, number, (arg0: number) => void, () => void] {
+function useMusicPlayer(): [
+  Howl,
+  number,
+  number,
+  (arg0: number) => void,
+  () => void,
+  number,
+  (arg0: number) => void,
+  number,
+] {
   const dispatch = useDispatch();
   const [sound, setSound] = useState<Howl | null>();
   const [volume, setVolume] = useState<number>(0.5);
   const [mute, setMute] = useState<boolean>(false);
+  const [soundID, setSoundID] = useState<number>();
+  const [currentSoundPosition, setCurrentSoundPosition] = useState<number>();
+  const [songDuration, setSongDuration] = useState<number>();
   const isSoundPaused = useSelector((state: RootReducer) => state.musicPlayer.isPaused);
   const isSoundPlaying = useSelector((state: RootReducer) => state.musicPlayer.isPlaying);
   const soundURL = useSelector((state: RootReducer) => state.musicPlayer.soundURL);
+  let timer: number;
+
+  const handleCurrentSoundPosition = (value: number): void => {
+    // (sound as any)._sounds[0]._seek
+    console.log('value', value);
+    console.log('value but current', currentSoundPosition);
+    sound?.seek(value, soundID);
+    setCurrentSoundPosition(value);
+    console.log('value but current 2', currentSoundPosition);
+  };
+
+  const runTimer = (): void => {
+    timer = setInterval(() => {
+      console.log('ehh', currentSoundPosition);
+      setCurrentSoundPosition((value: number | undefined) => (value as number) + 1);
+      console.log('ehh 2', currentSoundPosition);
+    }, 1000);
+  };
+
+  const stopTimer = (): void => {
+    clearInterval(timer);
+  };
 
   const toggleSoundMute = (): void => {
     if (!mute) {
@@ -46,6 +80,14 @@ function useMusicPlayer(): [Howl, number, (arg0: number) => void, () => void] {
       onloaderror: (id, error): void => {
         dispatch(playMusicFailure(error));
       },
+      onplay: (id): void => {
+        console.log('run timer');
+        runTimer();
+      },
+      onpause: (id): void => {
+        console.log('stopped timer');
+        stopTimer();
+      },
       onplayerror: (id, error): void => {
         dispatch(playMusicFailure(error));
       },
@@ -54,7 +96,7 @@ function useMusicPlayer(): [Howl, number, (arg0: number) => void, () => void] {
   };
 
   const playSound = (): void => {
-    sound?.play();
+    setSoundID(sound?.play());
   };
 
   const pauseSound = (): void => {
@@ -64,6 +106,25 @@ function useMusicPlayer(): [Howl, number, (arg0: number) => void, () => void] {
   const stopSound = (): void => {
     sound?.stop();
   };
+
+  useEffect(() => {
+    return (): void => stopTimer();
+  }, []);
+
+  useEffect(() => {
+    console.log('eooo');
+    const time = Number(sound?.seek(soundID)) || 0;
+    setCurrentSoundPosition(time);
+    setSongDuration(sound?.duration(soundID));
+  }, [sound, soundID]);
+
+  useEffect(() => {
+    console.log('changed', currentSoundPosition);
+  }, [currentSoundPosition]);
+
+  useEffect(() => {
+    setSongDuration(sound?.duration(soundID) as number);
+  }, [sound]);
 
   useEffect(() => {
     stopSound();
@@ -79,7 +140,16 @@ function useMusicPlayer(): [Howl, number, (arg0: number) => void, () => void] {
     }
   }, [isSoundPaused, isSoundPlaying]);
 
-  return [sound as Howl, volume, handleVolume, toggleSoundMute];
+  return [
+    sound as Howl,
+    soundID as number,
+    volume,
+    handleVolume,
+    toggleSoundMute,
+    currentSoundPosition as number,
+    handleCurrentSoundPosition,
+    songDuration as number,
+  ];
 }
 
 export default useMusicPlayer;
