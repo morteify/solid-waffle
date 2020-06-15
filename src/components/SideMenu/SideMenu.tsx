e64180ed8c6c0ff04901c40da7bb0eb29c72e6d4
morteify <michal.weglarz@protonmail.com>
2020-06-15 10:01:45 +0200

    Remove obsolete comments

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom';
 function SideMenu() {
   const history = useHistory();
 
-  const handleClick = (event: ClickParam) => {
+  const handleClick = (event: ClickParam): void => {
     history.push(`/${event.key}`);
   };
 
@@ -19,21 +19,10 @@ function SideMenu() {
       mode="inline"
     >
       <Menu.ItemGroup key="g1" title="Browse Music">
-        {/* <Menu.Item key="home">Home</Menu.Item> */}
         <Menu.Item key="songs">Songs</Menu.Item>
         <Menu.Item key="albums">Albums</Menu.Item>
         <Menu.Item key="artists">Artists</Menu.Item>
       </Menu.ItemGroup>
-      {/* <Menu.ItemGroup key="g2" title="Your Music">
-        <Menu.Item key="favourites">Favourites</Menu.Item>
-        <Menu.Item key="history">History</Menu.Item>
-      </Menu.ItemGroup>
-      <Menu.ItemGroup key="g3" title="Your Playlists">
-        <Menu.Item key="7">Playlist 1</Menu.Item>
-        <Menu.Item key="8">Playlist 2</Menu.Item>
-        <Menu.Item key="9">Playlist 3</Menu.Item>
-        <Menu.Item key="10">Playlist 4</Menu.Item>
-      </Menu.ItemGroup> */}
     </Menu>
   );
 }
